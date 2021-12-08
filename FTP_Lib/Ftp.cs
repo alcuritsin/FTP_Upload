b39fd7a0f65ee7e0aab15f66f4a53eb500cd2804
Starinin Andrey <starininandrey@gmail.com>
2021-12-08 18:29:10 +0300

    Init FTP_Lib

---
+++ FTP_Lib/Ftp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace FTP_Lib
+{
+    public class Ftp
+    {
+        private readonly string _ftpPath;
+
+        public Ftp(string ftpPath)
+        {
+            _ftpPath = ftpPath;
+        }
+
+        public void UploadFile(string filePath, out string responseStatus)
+        {
+            var request = (FtpWebRequest)WebRequest.Create(_ftpPath);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            
+            using var fs = new FileStream(filePath, FileMode.Open);
+            var fileContents = new byte[fs.Length];
+            fs.Read(fileContents, 0, fileContents.Length);
+            request.ContentLength = fileContents.Length;
+            
+            using var requestStream = request.GetRequestStream();
+            requestStream.Write(fileContents, 0, fileContents.Length);
+
+            using var response = (FtpWebResponse)request.GetResponse();
+            responseStatus = response.StatusDescription;
+        }
+    }
+}
+
