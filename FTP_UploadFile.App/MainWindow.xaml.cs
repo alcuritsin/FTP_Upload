443d826f97178181a47eeab69207ba1c1d74049b
Starinin Andrey <starininandrey@gmail.com>
2021-12-08 18:16:36 +0300

    Init proj

---
+++ FTP_UploadFile.App/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace FTP_UploadFile.App
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
