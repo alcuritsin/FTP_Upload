4804b5a6ad8436b82da7893d6ff0b93c772d0426
Starinin Andrey <starininandrey@gmail.com>
2021-12-08 18:46:44 +0300

    Добавление загрузки файла на FTP

--- FTP_UploadFile.App/MainWindow.xaml.cs
+++ FTP_UploadFile.App/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using FTP_Lib;
+using Microsoft.Win32;
 
 namespace FTP_UploadFile.App
 {
@@ -7,6 +10,31 @@ namespace FTP_UploadFile.App
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clear_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Button_SelectFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            Label_FilePath.Content = openFileDialog.FileName;
+        }
+
+        private void Button_Upload_OnClick(object sender, RoutedEventArgs e)
+        {
+            var filePath = Label_FilePath.Content.ToString();
+            if (!File.Exists(filePath)) return;
+
+            var fileName = Path.GetFileName(filePath);
+            
+            var ftp = new Ftp($"{Input_AddressFTPServer}/{fileName}");
+            ftp.UploadFile(filePath, out var status);
+
+            MessageBox.Show(status);
         }
     }
 }
