9dc85a9fc3a37d333e788dc828e27b89bb584f85
Starinin Andrey <starininandrey@gmail.com>
2021-12-09 12:33:40 +0300

    Добавление документирования кода

--- FTP_Lib/Ftp.cs
+++ FTP_Lib/Ftp.cs
@@ -3,12 +3,21 @@ using System.Net;
 
 namespace FTP_Lib
 {
+    /// <summary>
+    /// Основной класс библиотеки для работы с FTP
+    /// </summary>
     public class Ftp
     {
         private readonly string _ftpPath;
         private readonly string _login;
         private readonly string _password;
 
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="ftpPath">Путь к FTP-серверу</param>
+        /// <param name="login">Логин пользователя</param>
+        /// <param name="password">Пароль пользователя</param>
         public Ftp(string ftpPath, string login, string password)
         {
             _ftpPath = ftpPath;
@@ -16,6 +25,11 @@ namespace FTP_Lib
             _password = password;
         }
 
+        /// <summary>
+        /// Загрузка любого файла на FTP-сервер
+        /// </summary>
+        /// <param name="filePath">Путь к файлу</param>
+        /// <param name="responseStatus">Ответ FTP-сервера</param>
         public void UploadFile(string filePath, out string responseStatus)
         {
             var request = (FtpWebRequest)WebRequest.Create(_ftpPath);
